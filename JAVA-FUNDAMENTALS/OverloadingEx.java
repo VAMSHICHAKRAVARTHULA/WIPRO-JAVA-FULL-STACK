c09b8f2350d4c95401aeed16972d45a861e7bc45
VAMSHI <vamshichakravarthula05@gmail.com>
2025-07-18 12:01:20 +0530

    Add files via upload

---
+++ JAVA-FUNDAMENTALS/OverloadingEx.java
@@ -0,0 +1,25 @@
+class calculator 
+{
+    int add(int a, int b)
+    {
+        return a+b;
+    }
+    int add(int a, int b, int c )
+    {
+        return a+b+c;
+    }
+    double add(double a, double b)
+    {
+        return a+b;
+    }
+     public class overloadingexample
+    {
+     public static void main(String[] args) 
+    {
+        calculator calc= new calculator();
+        System.out.println("Add(2,3):" + calc.add(2,3));
+         System.out.println("Add(2,3,4):" + calc.add(2,3,4));
+          System.out.println("Add(2.5,3.5):" + calc.add(2.5,3.5));
+    }
+}
+}
