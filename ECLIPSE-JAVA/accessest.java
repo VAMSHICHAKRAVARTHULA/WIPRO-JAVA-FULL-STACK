af6cfdf1d358d3f28aceebe13cf86983d48e3fba
VAMSHI <vamshichakravarthula05@gmail.com>
2025-07-18 14:10:33 +0530

    Add files via upload

---
+++ ECLIPSE-JAVA/accessest.java
@@ -0,0 +1,13 @@
+package other;
+import test.Foundation;
+
+public class accessest {
+	
+
+	public static void main(String[] args) {
+		Foundation f=new Foundation();
+		System.out.println("var4:"+f.var4);
+
+	}
+
+}
